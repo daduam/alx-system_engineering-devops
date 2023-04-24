35e0f8835aade29a87c0b89875993b7c1395538e
Joseph Ampadu <jkampadujnr@gmail.com>
2023-04-24 22:42:55 +0000

    Puppet install nginx web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,41 @@
 # Install and configure nginx web server
 
-exec { 'nginx':
-  provider => shell,
-  command  => 'sudo apt-get update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart'
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!'
+}
+
+file { '/var/www/html/404.html':
+  content => 'Ceci n\'est pas une page',
+}
+
+file { '/etc/nginx/sites-available/default':
+  content => "
+server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
+
+  root /var/www/html;
+  index index.html;
+
+  error_page 404 /404.html;
+  location = /404.html {
+    root /var/www/html;
+    internal;
+  }
+
+  rewrite ^/redirect_me https://google.com permanent;
+}
+"
+}
+
+exec { 'Restart nginx':
+  command => '/usr/sbin/service nginx restart',
 }
