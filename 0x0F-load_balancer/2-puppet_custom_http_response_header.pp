842431182f2ab5d760e4af559034e0c6f478766d
Joseph Ampadu <jkampadujnr@gmail.com>
2023-05-02 14:50:54 +0000

    Setup nginx on web server with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,43 @@
+# Install and configure nginx web server
+
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!'
+}
+
+file { '/var/www/html/404.html':
+  content => 'Ceci n\'est pas une page',
+}
+
+file { '/etc/nginx/sites-available/default':
+  content => "
+server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
+
+  root /var/www/html;
+  index index.html;
+
+  error_page 404 /404.html;
+  location = /404.html {
+    root /var/www/html;
+    internal;
+  }
+
+  rewrite ^/redirect_me https://google.com permanent;
+
+  add_header X-Served-By \"${hostname}\";
+}
+"
+}
+
+exec { 'Restart nginx':
+  command => '/usr/sbin/service nginx restart',
+}
